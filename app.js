ca6def0fbb413e6c6c726bdbbde1369d07f379b0
Sachin Thakur <sachinst000@mail.com>
2020-03-13 10:38:14 +0530

    final changes added to the pig game

--- app.js
+++ app.js
@@ -9,59 +9,60 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+var scores, roundScore, activePlayer, isGamePlaying;
 
 //document.querySelector("#current-" + activePlayer).textContent = dice;
 
 /* lecture-48 <em> is an html element. written in "". else it is taken as js code
  with innerHTML we hav used <em> to emphasize the dice value.*/
-document.querySelector(".dice").style.display = "none";
-document.getElementById("score-0").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+initializeGame();
 //----------- lecture 49 --------------->>>>
 // event listener on Roll button
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  var dice = Math.floor(Math.random() * 6) + 1;
-  var diceDOM = document.querySelector(".dice");
-  diceDOM.style.display = "block";
-  diceDOM.src = `dice-${dice}.png`; //to select dice image on the basis of random number. see index file <img src=''>
+  if (isGamePlaying) {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = `dice-${dice}.png`; //to select dice image on the basis of random number. see index file <img src=''>
 
-  if (dice !== 1) {
-    roundScore += dice;
-    document.querySelector("#current-" + activePlayer).innerHTML =
-      "<em>" + roundScore + "</em>";
-    /* +activePlayer is used to select player 1 or player 2 and add score to the active player only.
- textContent this will only set the plain text to html element.
- if you also want to some html to set on the selected element use (innerHTML).*/
-  } else {
-    changePlayersTurn();
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector("#current-" + activePlayer).innerHTML =
+        "<em>" + roundScore + "</em>";
+      /* +activePlayer is used to select player 1 or player 2 and add score to the active player only.
+   textContent this will only set the plain text to html element.
+   if you also want to some html to set on the selected element use (innerHTML).*/
+    } else {
+      changePlayersTurn();
+    }
   }
 });
 
 // lecture 51
 // event listener on Hold button
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  scores[activePlayer] += roundScore;
-  document.querySelector(`#score-${activePlayer}`).textContent =
-    scores[activePlayer]; //settings global score
-  if (scores[activePlayer] >= 10) {
-    document.querySelector(`#name-${activePlayer}`).textContent = "WINNER!";
-    document.querySelector(".dice").style.display = "none";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else {
-    changePlayersTurn();
+  if (isGamePlaying) {
+    scores[activePlayer] += roundScore;
+    document.querySelector(`#score-${activePlayer}`).textContent =
+      scores[activePlayer]; //settings global score
+    if (scores[activePlayer] >= 10) {
+      document.querySelector(`#name-${activePlayer}`).textContent = "WINNER!";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      isGamePlaying = false;
+    } else {
+      changePlayersTurn();
+    }
   }
 });
+
+// lecture 52---->>>> new game button event implementation
+document.querySelector(`.btn-new`).addEventListener("click", initializeGame);
 
 function changePlayersTurn() {
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
@@ -73,4 +74,23 @@ function changePlayersTurn() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
   document.querySelector(".dice").style.display = "none";
+}
+
+function initializeGame() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  isGamePlaying = true;
+  document.querySelector(".dice").style.display = "none";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById(`name-0`).textContent = "PLAYER 1";
+  document.getElementById(`name-1`).textContent = "PLAYER 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
 }
